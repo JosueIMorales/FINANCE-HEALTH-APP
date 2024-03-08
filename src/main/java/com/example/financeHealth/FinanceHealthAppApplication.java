33dac2473e7afa54493087bfb4e5c9771cdb6a14
Josue <josueisraelmt21@gmail.com>
2024-03-08 16:37:48 -0600

    First Commit

---
+++ src/main/java/com/example/financeHealth/FinanceHealthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.financeHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceHealthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceHealthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financeHealth/FinanceHealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financeHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceHealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
